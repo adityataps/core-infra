d47125d1a6d34f1ebee38612e661af2030acdeec
Aditya Tapshalkar <aditya.taps@gmail.com>
2026-04-16 21:45:06 -0400

    refactor(aws/org): manage OUs only — remove aws_organizations_account resources

--- providers/aws/org/outputs.tf
+++ providers/aws/org/outputs.tf
@@ -1,6 +1,6 @@
 output "root_id" {
   description = "ID of the organization root"
-  value       = data.aws_organizations_organization.this.roots[0].id
+  value       = aws_organizations_organization.this.roots[0].id
 }
 
 output "personal_ou_id" {
@@ -18,22 +18,7 @@ output "projects_ou_id" {
   value       = aws_organizations_organizational_unit.projects.id
 }
 
-output "personal_account_id" {
-  description = "AWS account ID of the new personal member account"
-  value       = aws_organizations_account.personal.id
-}
-
-output "certs_account_1_id" {
-  description = "AWS account ID of the first certs account"
-  value       = aws_organizations_account.certs_1.id
-}
-
-output "certs_account_2_id" {
-  description = "AWS account ID of the second certs account"
-  value       = aws_organizations_account.certs_2.id
-}
-
-output "side_project_account_id" {
-  description = "AWS account ID of the side-project account"
-  value       = aws_organizations_account.side_project.id
+output "management_ou_id" {
+  description = "ID of the management/ OU"
+  value       = aws_organizations_organizational_unit.management.id
 }
